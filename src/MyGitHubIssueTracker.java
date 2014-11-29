0908d6f0e7919590c763b19cba658603041adf74
ZiXian92 <zixian1992@hotmail.com>
2014-11-29 22:18:40 +0800

    First commit

---
+++ src/MyGitHubIssueTracker.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class MyGitHubIssueTracker {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	UI ui = new UI();
+	
+	ui.run();
+    }
+    
+}
