5dd4409f4bc862e13824d315760ed7a6a3cfc930
zixian92 <zixian1992@hotmail.com>
2014-12-17 15:59:54 +0800

    Adds CloseIssue class

---
+++ src/controller/CloseIssue.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class CloseIssue extends Command {
+	//Data members
+	private String repoName, issueName;
+	
+	/**
+	 * Creates a new instance of this command to close an issue.
+	 * @param repoName The name of the repository in which to close the issue. Cannot be null or an empty string.
+	 * @param issueName The issue to close. Cannot be null or an empty string.
+	 */
+	public CloseIssue(String repoName, String issueName){
+		assert repoName!=null && !repoName.isEmpty() && issueName!=null && !issueName.isEmpty();
+		this.repoName = repoName;
+		this.issueName = issueName;
+	}
+
+	@Override
+	public void execute() throws Exception {
+		model.closeIssue(repoName, issueName);
+	}
+
+}
