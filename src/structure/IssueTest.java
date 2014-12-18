ea1f4f26762b638522fed9aad8ba04e0c12aa6ce
zixian92 <zixian1992@hotmail.com>
2014-12-18 13:14:12 +0800

    Adds released JAR files

---
+++ src/structure/IssueTest.java
@@ -0,0 +1,19 @@
+package structure;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * JUnit test clsss to test Issue class.
+ * @author ZiXian92
+ */
+public class IssueTest {
+	//Note to self: Use curl to download an issue in json format in a file.
+	//Get the JSON string from the file to test issue class.
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
