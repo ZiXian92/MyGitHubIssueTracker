fe4ea5a71509b6616c7684823a3a65327cb65625
zixian92 <zixian1992@hotmail.com>
2014-12-22 00:05:17 +0800

    Implements constructor for EditIssue

--- src/controller/EditIssue.java
+++ src/controller/EditIssue.java
@@ -1,9 +1,17 @@
 package controller;
 
 public class EditIssue extends Command {
+	private String repoName, issueName;
 	
+	/**
+	 * Creates a new Command instance to edit the given issue in the given repository.
+	 * @param repoName The name of the repository from which to edit the issue. Cannot be null or empty string.
+	 * @param issueName The name of the issue to edit. Cannot be null or empty string.
+	 */
 	public EditIssue(String repoName, String issueName){
-		
+		assert repoName!=null && !repoName.isEmpty() && issueName!=null && !issueName.isEmpty();
+		this.repoName = repoName;
+		this.issueName = issueName;
 	}
 
 	@Override
