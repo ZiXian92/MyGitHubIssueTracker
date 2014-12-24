17cbc65262bd6901ae6baa063c0a7f95d857645f
zixian92 <zixian1992@hotmail.com>
2014-12-24 14:19:52 +0800

    Adds unit test to LoadLabelsThread

---
+++ src/model/LoadLabelsThreadTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.client.methods.HttpGet;
+import org.junit.Test;
+
+import structure.Repository;
+
+/**
+ * JUnit test to test LoadLabelsThread class.
+ * @author ZiXian92
+ */
+public class LoadLabelsThreadTest {
+
+	@Test
+	public void test() {
+		Repository repo = new Repository("MyGitHubIssueTracker", "zixian92");
+		HttpGet req = new HttpGet("https://api.github.com/repos/zixian92/MyGitHubIssueTracker/labels");
+		req.addHeader("Accept", "application/vnd.github.v3+json");
+		LoadLabelsThread thread = new LoadLabelsThread(repo, req);
+		thread.run();
+		assertEquals(11, repo.getLabels().size());
+	}
+
+}
