0315859e7054d9156dce66de472e54e8b00d0b03
zixian92 <zixian1992@hotmail.com>
2014-12-24 16:21:59 +0800

    Implements editing of labels together with edit issue

--- src/controller/EditIssue.java
+++ src/controller/EditIssue.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,11 +17,15 @@ public class EditIssue extends Command {
 	private static final String KEY_TITLE = "title";
 	private static final String KEY_BODY = "body";
 	private static final String KEY_ASSIGNEE = "assignee";
+	private static final String KEY_LABELS = "labels";
 	
 	//Prompt messages
 	private static final String PROMPT_TITLE = "New title(Enter nothing to retain current title): ";
 	private static final String PROMPT_CONTENT = "New content(Enter nothing to skip, space to remove content): ";
 	private static final String PROMPT_ASSIGNEE = "New assignee(Enter nothing to skip, space to remove assignee): ";
+	private static final String PROMPT_LABELS = "Labels(Enter nothing to skip, comma-separated labels to replace current labels): ";
+	
+	private static final String LABEL_DELIM = ",";
 	
 	//Error messages
 	private static final String MSG_PARSEERROR = "Error parsing changes.";
@@ -60,6 +65,17 @@ public class EditIssue extends Command {
 			if(!(input = reader.readLine()).isEmpty()){
 				input = input.trim();
 				obj.put(KEY_ASSIGNEE, input.isEmpty()? JSONObject.NULL: input);
+			}
+			printPrompt(PROMPT_LABELS);
+			if(!(input = reader.readLine()).isEmpty()){
+				obj.put(KEY_LABELS, new JSONArray());
+				if(!(input = input.trim()).isEmpty()){
+					String[] labels = input.split(LABEL_DELIM);
+					int numLabels = labels.length;
+					for(int i=0; i<numLabels; i++){
+						obj.append(KEY_LABELS, labels[i].trim());
+					}
+				}
 			}
 		} catch(JSONException e){
 			throw new JSONException(MSG_PARSEERROR);
