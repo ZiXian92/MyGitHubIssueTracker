50b96dab5c7a4863a7a793db2784235a32fcb425
zixian92 <zixian1992@hotmail.com>
2015-01-01 12:37:42 +0800

    Creates method to send request to GitHub API in Util

--- src/Misc/Util.java
+++ src/Misc/Util.java
@@ -6,6 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPatch;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClients;
+
 /**
  * Defines the utility class of commonly-used methods in this application.
  * @author ZiXian92
@@ -17,7 +25,7 @@ public class Util {
 	/**
 	 * List of allowed Http requests for this class.
 	 */
-	public enum HttpRequestType{ GET, POST, PATCH };
+	private enum HttpRequestType{ GET, POST, PATCH };
 	
 	/**
 	 * Reads the JSON string from the message body of the given HTTP response.
@@ -56,5 +64,49 @@ public class Util {
 			strBuilder = strBuilder.append(ITEM_DELIM).append(list.get(i));
 		}
 		return strBuilder.toString();
+	}
+	
+	/**
+	 * Sends a Http GET request to the given URL.
+	 * @param url The URL of the API method to call. Cannot be null.
+	 * @param authCode The Base64-encoded string of username and password to be used for basic authentication.
+	 * 					Cannot be null.
+	 * @throws IOException If an error occurred during the request.
+	 * */
+	public static CloseableHttpResponse sendGetRequest(String url, String authCode) throws IOException{
+		return sendRequest(HttpRequestType.GET, url, authCode, null);
+	}
+	
+	/**
+	 * Sends a Http request to GitHub API using the given paramters.
+	 * @param reqType The type of Http request to be sent. Cannot be null.
+	 * @param url The URL of the API method to be called. Cannot be null.
+	 * @param authCode The Base64-encoded string of username and password for basic authentication.
+	 * @param msg The message entity to be used in POST or PATCH requests.
+	 * @return The corresponding Http response for the request.
+	 * @throws IOException If an error occurred during the request.
+	 */
+	private static CloseableHttpResponse sendRequest(HttpRequestType reqType,
+			String url, String authCode, HttpEntity msg) throws IOException {
+		assert reqType!=null && url!=null && authCode!=null;
+		HttpUriRequest req;
+		switch(reqType){
+			case GET: req = new HttpGet(url);
+					break;
+			case POST: req = new HttpPost(url);
+					if(msg!=null){
+						((HttpPost)req).setEntity(msg);
+					}
+					break;
+			case PATCH: req = new HttpPatch(url);
+					if(msg!=null){
+						((HttpPatch)req).setEntity(msg);
+					}
+					break;
+			default: return null;	//will not happen
+		}
+		req.addHeader(Constants.HEADER_ACCEPT, Constants.VAL_ACCEPT);
+		req.addHeader(Constants.HEADER_AUTH, String.format(Constants.VAL_AUTH, authCode));
+		return HttpClients.createDefault().execute(req);
 	}
 }
